c15c06edf771830b1d55105127d8dafe26433432
sanorcdel <santyorcajo@gmail.com>
2015-11-17 17:48:45 +0100

    Incluidos ficheros ejemplo.py y rectangulo.py
    Se han incluido los ficheros en lenguaje Python rectangulo.py y
    ejemplo.py.

---
+++ ejemplo.py
@@ -0,0 +1,4 @@
+from rectangulo import rectangulo
+
+x = rectangulo(1, 2)
+print x.area()
---
+++ rectangulo.py
@@ -0,0 +1,7 @@
+class rectangulo:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def area(self):
+		return self.x * self.y
